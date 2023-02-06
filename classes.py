d567b5f9a8c35df71a34e034fe2e0f1decc1456d
Monduli <dcglendon@gmail.com>
2023-02-06 15:48:55 -0500

    Added spread function to classes, added two more classes

--- classes.py
+++ classes.py
@@ -55,10 +55,57 @@ class Character:
     def set_cha(self, value):
         self.charisma = value
 
+    def distribute_stats(self, spread, stats):
+        temp = stats
+        for i in range(6):
+            if spread[i] == "str":
+                self.set_str(max(temp))
+                stats.remove(max(temp))
+            elif spread[i] == "dex":
+                self.set_dex(max(temp))
+                stats.remove(max(temp))
+            elif spread[i] == "con":
+                self.set_con(max(temp))
+                stats.remove(max(temp))
+            elif spread[i] == "int":
+                self.set_int(max(temp))
+                stats.remove(max(temp))
+            elif spread[i] == "wis":
+                self.set_wis(max(temp))
+                stats.remove(max(temp))
+            elif spread[i] == "cha":
+                self.set_cha(max(temp))
+                stats.remove(max(temp))
+
+
 class Fighter(Character):
-    def __init__(self) -> None:
+    def __init__(self, stats) -> None:
         super().__init__()
+        self.stats = stats
+
+    def spread(stats):
+        Character.distribute_stats(["str", "con", "dex", "wis", "cha", "int"], stats)
 
 class Paladin(Character):
-    def __init__(self) -> None:
+    def __init__(self, stats) -> None:
         super().__init__()
+        self.stats = stats
+
+    def spread(stats):
+        Character.distribute_stats(["cha", "str", "con", "dex", "wis", "int"], stats)
+
+class Wizard(Character):
+    def __init__(self, stats) -> None:
+        super().__init__()
+        self.stats = stats
+
+    def spread(stats):
+        Character.distribute_stats(["int", "con", "dex", "wis", "cha", "str"], stats)
+
+class Ranger(Character):
+    def __init__(self, stats) -> None:
+        super().__init__()
+        self.stats = stats
+
+    def spread(stats):
+        Character.distribute_stats(["dex", "wis", "con", "str", "cha", "int"], stats)
