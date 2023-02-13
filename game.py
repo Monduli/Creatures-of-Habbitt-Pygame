27143202f10191e0065a3c6fca8f167af49788af
Monduli <glendontus@gmail.com>
2023-02-13 17:33:24 -0500

    Added title screen, mouseover buttons works, exit button works, wake up takes you to dialog system

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 import sys, pygame
 import dialog as dia
 
-def drawText(surface, text, color, rect, font, aa=False, bkg=None):
+def drawText(surface, text, color, rect, font, aa=False, bkg=None, center=False):
     rect = pygame.Rect(rect)
     y = rect.top
     lineSpacing = 0
@@ -30,14 +30,88 @@ def drawText(surface, text, color, rect, font, aa=False, bkg=None):
             image.set_colorkey(bkg)
         else:
             image = font.render(text[:i], aa, color)
-
-        surface.blit(image, (rect.left+20, y+10))
+        text_rect = image.get_rect()
+        if center == True:
+            text_rect.center = rect.center
+            surface.blit(image, text_rect)
+        else:
+            surface.blit(image, (rect.left+20, y+10))
         y += fontHeight + lineSpacing
 
         # remove the text we just blitted
         text = text[i:]
 
     return text
+
+def start_screen():
+
+    #### SETUP ####
+    pygame.init()
+
+    size = width, height = 1600, 900
+    black = 0, 0, 0
+    speed = [3, 0]
+
+    clock = pygame.time.Clock()
+
+    screen = pygame.display.set_mode(size)
+
+    base_font = pygame.font.Font("VCR.001.ttf", 32)
+
+    color_passive = pygame.Color('black')
+
+    background = pygame.image.load("cave.png")
+    background = pygame.transform.scale(background,(1600,900))
+
+    title_rect = pygame.Rect(width-1000,height-800,400,50)
+    start_rect = pygame.Rect(width-900,height-550,200,50)
+    options_rect = pygame.Rect(width-900,height-400,200,50)
+    exit_rect = pygame.Rect(width-850,height-250,100,50)
+
+    i = 0
+
+    while True:
+        screen.fill(black)
+        screen.fill(black)
+        screen.blit(background, (width+i,0))
+        screen.blit(background, (i, 0))
+        color_start = pygame.Color('black')
+        color_options = pygame.Color('black')
+        color_exit = pygame.Color('black')
+
+        if (i == -width):
+            screen.blit(background, (width+i, 0))
+            i=0
+        i-=1
+        for event in pygame.event.get():                  
+            if event.type == pygame.QUIT: sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if start_rect.collidepoint(event.pos):
+                    return "dialog"
+                if exit_rect.collidepoint(event.pos):
+                    return "exit"
+
+        if start_rect.collidepoint(pygame.mouse.get_pos()):
+            color_start = pygame.Color(200,200,200)
+        if options_rect.collidepoint(pygame.mouse.get_pos()):
+            color_options = pygame.Color(200,200,200)
+        if exit_rect.collidepoint(pygame.mouse.get_pos()):
+            color_exit = pygame.Color(200,200,200)
+
+        # Draw buttons
+        pygame.draw.rect(screen, color_passive, title_rect)
+        pygame.draw.rect(screen, color_start, start_rect)
+        pygame.draw.rect(screen, color_options, options_rect)
+        pygame.draw.rect(screen, color_exit, exit_rect)
+        
+        # Draw the text onto the buttons
+        drawText(screen, "Dan's Game v.00", pygame.Color(255,255,255,0), title_rect, base_font, False, None, center=True)
+        drawText(screen, "Wake Up", pygame.Color(255,255,255,0), start_rect, base_font, False, None, center=True)
+        drawText(screen, "Options", pygame.Color(255,255,255,0), options_rect, base_font, False, None, center=True)
+        drawText(screen, "Exit", pygame.Color(255,255,255,0), exit_rect, base_font, False, None, center=True)
+
+        pygame.display.update()
+        clock.tick(60)
 
 def in_dialog():
 
@@ -90,64 +164,6 @@ def in_dialog():
         #screen.blit(text_surface, (input_rect.x+20, input_rect.y+20))
         #input_rect.w = max(1500, text_surface.get_width()+10)
         drawText(screen, user_text[advance], (255,255,255), dialog_rect, base_font)
-
-        pygame.display.update()
-        clock.tick(60)
-
-def start_screen():
-
-    #### SETUP ####
-    pygame.init()
-
-    size = width, height = 1600, 900
-    speed = [3, 0]
-    black = 0, 0, 0
-
-    clock = pygame.time.Clock()
-
-    screen = pygame.display.set_mode(size)
-
-    background = pygame.image.load("cave.png")
-    background = pygame.transform.scale(background,(1600,900))
-
-    base_font = pygame.font.Font("VCR.001.ttf", 32)
-
-    color_passive = pygame.Color('black')
-
-    title_rect = pygame.Rect(width-1550,height-800,300,50)
-    start_rect = pygame.Rect(width-1550,height-650,300,100)
-    options_rect = pygame.Rect(width-1550,height-400,300,100)
-    exit_rect = pygame.Rect(width-1550,height-250,300,100)
-
-    i = 0
-    while True:
-        screen.fill(black)
-        screen.blit(background, (width+i,0))
-        screen.blit(background, (i, 0))
-        if (i == -width):
-            screen.blit(background, (width+i, 0))
-            i=0
-        i-=1
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT: sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if start_rect.collidepoint(event.pos):
-                    return "dialog"
-                if exit_rect.collidepoint(event.pos):
-                    return "exit"
-
-        pygame.draw.rect(screen, color_passive, title_rect)
-        pygame.draw.rect(screen, color_passive, start_rect)
-        pygame.draw.rect(screen, color_passive, options_rect)
-        pygame.draw.rect(screen, color_passive, exit_rect)
-        #text_surface = base_font.render(user_text, True, (255,255,255))
-        #screen.blit(text_surface, (input_rect.x+20, input_rect.y+20))
-        #input_rect.w = max(1500, text_surface.get_width()+10)
-        
-        drawText(screen, "Dan's Game v.00", (255,255,255), title_rect, base_font)
-        drawText(screen, "Wake Up", (255,255,255), start_rect, base_font)
-        drawText(screen, "Options", (255,255,255), options_rect, base_font)
-        drawText(screen, "Exit", (255,255,255), exit_rect, base_font)
 
         pygame.display.update()
         clock.tick(60)
