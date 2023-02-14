d53b400ebdb02ba8c45743fc0b83272a4dc6c6da
Monduli <glendontus@gmail.com>
2023-02-14 10:39:53 -0500

    Added name input dialog, centered no matter what you type into it. Fixed options and how dialog loads

--- dialog.py
+++ dialog.py
@@ -1,5 +1,35 @@
-dialog_1 = [
+#############
+# FORMAT:
+# [0] - Dialog in order
+# [1] - Target of left choice button at intersection
+# [2] - Target of right choice button at intersection
+#############
+name = "Default"
+
+def determine_dialog(target, name="Default"):
+    match target:
+        case "dialog_start_2":
+            dialog_start_2[0][0] = "Your name is "+ name +"."
+            dialog_start_2[0][1] = "Hello, "+name+"!"
+            return dialog_start_2
+
+dialog_start = [[
     """Dan's game version 0.0""",
     """You find yourself in a dark cave.""",
-    """You may go left or right."""
-]
+    """You try to remember your name.""",
+    "Please type into the box.",
+], "dialog_start_2"]
+
+dialog_start_2 = [[
+    "Your name is name.",
+    "Hello name!",
+    "Let's get you out of this cave.",
+    "Please select an option.",
+    "Exit Cave",
+    "Stick Around"
+], "cave_exit", "meander"]
+
+dialog_right = [[
+    "You chose to go right.",
+    "There is nothing here."
+]]
--- game.py
+++ game.py
@@ -1,10 +1,11 @@
 import sys, pygame
 import dialog as dia
 
-def drawText(surface, text, color, rect, font, aa=False, bkg=None, center=False):
+def drawText(surface, text, color, rect, font, aa=False, bkg=None, center=False, input=False):
     rect = pygame.Rect(rect)
     y = rect.top
     lineSpacing = 0
+    image = None
 
     # get the height of the font
     fontHeight = font.size("Tg")[1]
@@ -41,6 +42,8 @@ def drawText(surface, text, color, rect, font, aa=False, bkg=None, center=False)
         # remove the text we just blitted
         text = text[i:]
 
+    if input == True:
+        return image
     return text
 
 def start_screen():
@@ -92,11 +95,11 @@ def start_screen():
                     return "exit"
 
         if start_rect.collidepoint(pygame.mouse.get_pos()):
-            color_start = pygame.Color(200,200,200)
+            color_start = pygame.Color(200,0,0)
         if options_rect.collidepoint(pygame.mouse.get_pos()):
-            color_options = pygame.Color(200,200,200)
+            color_options = pygame.Color(200,0,0)
         if exit_rect.collidepoint(pygame.mouse.get_pos()):
-            color_exit = pygame.Color(200,200,200)
+            color_exit = pygame.Color(200,0,0)
 
         # Draw buttons
         pygame.draw.rect(screen, color_passive, title_rect)
@@ -128,7 +131,7 @@ def in_dialog():
     background = pygame.transform.scale(background,(1600,900))
 
     base_font = pygame.font.Font("VCR.001.ttf", 32)
-    user_text = dia.dialog_1
+    user_text = dia.dialog_start
 
     dialog_rect = pygame.Rect(width-1550,height-250,1500,200)
     color_passive = pygame.Color('black')
@@ -152,10 +155,17 @@ def in_dialog():
                 if dialog_rect.collidepoint(event.pos):
                     if exit_next == 1:
                         sys.exit()
-                    elif 0 <= advance+1 < len(user_text):
+                    elif user_text[0][advance] == "Please select an option.":
+                        choice = dialog_options(screen, user_text[0][advance+1], user_text[0][advance+2], user_text[1], user_text[2])
+                        user_text = dia.determine_dialog(choice)
+                        advance = 0
+                    elif user_text[0][advance] == "Please type into the box.":
+                        user_text = input_box(user_text[1])
+                        advance = 0
+                    elif 0 <= advance+1 < len(user_text[0]):
                         advance += 1
                     else:
-                        user_text = ["End of text."]
+                        user_text = [["End of text."]]
                         exit_next = 1
                         advance = 0
 
@@ -163,9 +173,156 @@ def in_dialog():
         #text_surface = base_font.render(user_text, True, (255,255,255))
         #screen.blit(text_surface, (input_rect.x+20, input_rect.y+20))
         #input_rect.w = max(1500, text_surface.get_width()+10)
-        drawText(screen, user_text[advance], (255,255,255), dialog_rect, base_font)
+        drawText(screen, user_text[0][advance], (255,255,255), dialog_rect, base_font)
 
         pygame.display.update()
+        clock.tick(60)
+
+def dialog_options(screen, text_left, text_right, target_left, target_right):
+    size = width, height = 1600, 900
+    clock = pygame.time.Clock()
+    black = 0, 0, 0
+
+    screen = pygame.display.set_mode(size)
+
+    background = pygame.image.load("cave.png")
+    background = pygame.transform.scale(background,(1600,900))
+
+    base_font = pygame.font.Font("VCR.001.ttf", 32)
+
+    color_passive = pygame.Color('black')
+
+    left_rect = pygame.Rect(width-1550,height-250,700,50)
+    right_rect = pygame.Rect(width-750,height-250,700,50)
+
+    i = 0
+
+    while True:
+        screen.fill(black)
+        screen.blit(background, (width+i,0))
+        screen.blit(background, (i, 0))
+        color_left = pygame.Color('black')
+        color_right = pygame.Color('black')
+
+        if (i == -width):
+            screen.blit(background, (width+i, 0))
+            i=0
+        i-=1
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if left_rect.collidepoint(event.pos):
+                    return target_left
+                if right_rect.collidepoint(event.pos):
+                    return target_right
+            
+        if left_rect.collidepoint(pygame.mouse.get_pos()):
+            color_left = pygame.Color(200,0,0)
+        if right_rect.collidepoint(pygame.mouse.get_pos()):
+            color_right = pygame.Color(200,0,0)
+
+        pygame.draw.rect(screen, color_left, left_rect)
+        drawText(screen, text_left, (255,255,255), left_rect, base_font)
+        pygame.draw.rect(screen, color_right, right_rect)
+        drawText(screen, text_right, (255,255,255), right_rect, base_font)
+
+        pygame.display.update()
+        clock.tick(60)
+
+def input_box(target):
+    size = width, height = 1600, 900
+    clock = pygame.time.Clock()
+    black = 0, 0, 0
+
+    screen = pygame.display.set_mode(size)
+
+    background = pygame.image.load("cave.png")
+    background = pygame.transform.scale(background,(1600,900))
+
+    base_font = pygame.font.Font("VCR.001.ttf", 32)
+
+    color_passive = pygame.Color('black')
+
+    i = 0
+
+    user_text = ''
+    
+    # create rectangle
+    input_rect = pygame.Rect(width-750, height/2, 200, 50)
+    input_rect.center = (width/2, height/2)
+    
+    
+    # color_active stores color(lightskyblue3) which
+    # gets active when input box is clicked by user
+    color_active = pygame.Color('red')
+    
+    # color_passive store color(chartreuse4) which is
+    # color of input box.
+    color = color_passive
+    
+    active = False
+    
+    while True:
+        screen.fill(black)
+        screen.blit(background, (width+i,0))
+        screen.blit(background, (i, 0))
+
+        if (i == -width):
+            screen.blit(background, (width+i, 0))
+            i=0
+        i-=1
+        for event in pygame.event.get():
+    
+        # if user types QUIT then the screen will close
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+    
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if input_rect.collidepoint(event.pos):
+                    active = True
+                else:
+                    active = False
+    
+            if event.type == pygame.KEYDOWN:
+    
+                # Check for backspace
+                if event.key == pygame.K_BACKSPACE:
+    
+                    # get text input from 0 to -1 i.e. end.
+                    user_text = user_text[:-1]
+    
+                # Unicode standard is used for string
+                # formation
+                else:
+                    user_text += event.unicode
+                
+                if event.key == pygame.K_RETURN:
+                    return dia.determine_dialog(target, user_text[:-1])
+    
+        if active:
+            color = color_active
+        else:
+            color = color_passive
+            
+        # draw rectangle and argument passed which should
+        # be on screen
+        pygame.draw.rect(screen, color, input_rect)
+    
+        rect = drawText(screen, user_text, pygame.Color('white'), input_rect, base_font, center=True, input=True)
+        
+        # set width of textfield so that text cannot get
+        # outside of user's text input
+        if rect != None:
+            input_rect.w = max(100, rect.get_width()+10)
+            input_rect.center = (width/2, height/2)
+        
+        # display.flip() will update only a portion of the
+        # screen to updated, not full area
+        pygame.display.update()
+        
+        # clock.tick(60) means that for every second at most
+        # 60 frames should be passed.
         clock.tick(60)
 
 def controller():
